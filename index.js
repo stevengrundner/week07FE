c94306c0cc85bc4845ec1a65b07418496f3093c3
Steven Grundner <stevengrundner@Stevens-MacBook-Pro.local>
2024-03-11 08:23:50 -0600

    adding html and js files

---
+++ index.js
@@ -0,0 +1,41 @@
+//      1) Create an array called ages that contains the following values: 3, 9, 23, 64, 2, 8, 28, 93.
+
+ages = [3, 9, 23, 64, 2, 8, 28, 93];
+console.log(ages);
+
+//      a)  Programmatically subtract the value of the first element in the array from the value in the last element of the array.
+//          Do not use numbers to reference the last element, find it programmatically.
+//          ages[7] - ages[0] is not allowed!
+
+console.log(ages[ages.length -1] - (ages [0]));
+
+//      b)  Add a new age to your array and repeat the step above to ensure it is dynamic. (works for arrays of different lengths).
+
+ages = [3, 9, 23, 64, 2, 8, 28, 93, 100];
+
+console.log(ages[ages.length -1] - (ages [0]));
+
+//      c)  Use a loop to iterate through the array and calculate the average age.
+
+
+
+
+
+
+
+
+
+
+//      2)      Create an array called names that contains the following values: 'Sam', 'Tommy', 'Tim', 'Sally', 'Buck', 'Bob'.
+
+let names = ['Sam', 'Tommy', 'Tim', 'Sally', 'Buck', 'Bob'];
+
+//      2a)     Use a loop to iterate through the array and calculate the average number of letters per name.
+
+//      2b)     Use a loop to iterate through the array again and concatenate all the names together, separated by spaces.
+
+//      3)      How do you access the last element of any array?
+
+//      4)      How do you access the first element of any array?
+
+
